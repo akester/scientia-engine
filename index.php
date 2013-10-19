05813caa2fbb821eff779642928bf975b821dafa
Andrew Kester <akester@cox.net>
2013-10-18 22:04:21 -0500

    index: adding function to return HTTP status codes

--- index.php
+++ index.php
@@ -20,5 +20,99 @@
  *    along with Scientia.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
+/* 
+ * We roll our own HTTP Status code function since not all versions of PHP have
+ * the built in header function.
+ */
+function sendResponseCode($code) {
+	/*
+	 * Status Codes taken from
+	 * http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+	 */
+	$statusCodes = array(
+			/* 1XX Informational */
+			100 => 'Continue',
+			101 => 'Switching Protocols',
+			102 => 'Processing',
+			/* 2XX Success */
+			200 => 'OK',
+			201 => 'Created',
+			202 => 'Accepted',
+			203 => 'Non-Authoritative Information',
+			204 => 'No Content',
+			205 => 'Reset Content',
+			206 => 'Partial Content',
+			207 => 'Multi-Status',
+			208 => 'Already Reported',
+			226 => 'IM Used',
+			/* 3XX Redirection */
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			303 => 'See Other',
+			304 => 'Not Modified',
+			305 => 'Use Proxy',
+			306 => 'Switch Proxy',
+			307 => 'Temporary Redirect',
+			308 => 'Permanent Redirect',
+			/* 4XX Client Error */
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			406 => 'Not Acceptable',
+			407 => 'Proxy Authentication Required',
+			408 => 'Request Timeout',
+			409 => 'Conflict',
+			410 => 'Gone',
+			411 => 'Length Required',
+			412 => 'Precondition Failed',
+			413 => 'Request Entitiy Too Large',
+			414 => 'Request URI Too Long',
+			415 => 'Unsupported Media Type',
+			416 => 'Requested Range Not Satisfiable',
+			417 => 'Expectation Failed',
+			419 => 'Authentaction Timeout',
+			420 => 'Method Failure',
+			422 => 'Unprocessable Entity',
+			423 => 'Locked',
+			424 => 'Failed Dependency',
+			425 => 'Unordered Collection',
+			426 => 'Upgrade Required',
+			428 => 'Precondition Required',
+			429 => 'Too Many Requests',
+			431 => 'Request Header Fields Too Large',
+			444 => 'No Response',
+			449 => 'Retry With',
+			450 => 'Blocked By Windows Parental Controls',
+			451 => 'Unavailable For Legal Reasons',
+			494 => 'Request Header Too Large',
+			495 => 'Cert Error',
+			496 => 'No Cert',
+			497 => 'HTTP to HTTPS',
+			499 => 'Client Closed Request',
+			/* 5XX Server Error */
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+			502 => 'Bad Gateway',
+			503 => 'Service Unavailable',
+			504 => 'Gateway Timeout',
+			505 => 'HTTP Version Not Supported',
+			506 => 'Variant Also Negotiates',
+			507 => 'Insuffcient Storage',
+			508 => 'Loop Detected',
+			509 => 'Bandwidth Limit Exceeded',
+			510 => 'Not Extended',
+			511 => 'Network Authentication Required',
+			522 => 'Connection Timed Out',
+			598 => 'Network Read Timeout Error',
+			599 => 'Network Connect Time Error'
+		);
+	if (!key_exists($code, $statusCodes))
+		$code = 501;
+	$responseString = 'HTTP/1.1 ' . $code . ' ' . $statusCodes[$code];
+	header($responseString);
+}
 ?>
