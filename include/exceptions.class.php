481f443d013504313d67c697d1c21dda0ffd9c3c
Andrew Kester <akester@cox.net>
2013-10-19 06:55:25 -0500

    exceptions: adding doc line

--- include/exceptions.class.php
+++ include/exceptions.class.php
@@ -29,6 +29,8 @@ class ScientiaGenericException extends Exception {
 	public function __construct($message, $code = 0) {
 		parent::__construct($message, $code);
 	}
+	
+	/* Scientia Specific Exception Parameters */
 }
 
 /**
