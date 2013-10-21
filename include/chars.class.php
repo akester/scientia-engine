c63c26636f1e780d0a3b4ec65dbc65709864abc7
Andrew Kester <akester@cox.net>
2013-10-20 20:17:25 -0500

    chars: adding some UTF-16 chars

--- include/chars.class.php
+++ include/chars.class.php
@@ -35,8 +35,38 @@ class scientiaSpecialChars {
 			 * The name should be in all caps (for convention's sake) and will
 			 * have C_ prepended (ex C_TRIBAR).  Replace spaces with an _
 			 */
-			'TRIBAR' => '2261',
-			'ARROW' => '2192',
+			'INVERT_EXLAIM' => '00A1',
+			'CENT_SIGN' => '00A2',
+			'POUND_SIGN' => '00A3',
+			'CURRENCY_SIGN' => '00A4',
+			'YEN SIGN' => '00A5',
+			'BROKEN_BAR' => '00A6',
+			'SECTION_SIGN' => '00A7',
+			'DIAERESIS' => '00A8',
+			'COPYRIGHT_SIGN' => '00A9',
+			'FEMININE_ORDINAL_INDICATOR' => '00AA',
+			'LEFT_DOUBLE_ANGLE' => '00AB',
+			'NOT_SIGN' => '00AC',
+			'REGISTERED_SIGN' => '00AE',
+			'MARCRON' => '00AF',
+			'DEGREE_SIGN' => '00B0',
+			'PLUS_MINUS_SIGN' => '00B1',
+			'SUPERSCRIPT_TWO' => '00B2',
+			'SUPERSCRIPT_THREE' => '00B3',
+			'ACUTE_ACCENT' => '00B4',
+			'MICRO_SIGN' => '00B5',
+			'PILCROW_SIGN' => '00B6',
+			'MIDDLE_DOT' => '00B7',
+			'CEDILLA' => '00B8',
+			'SUPERSCRIPT_ONE' => '00B9',
+			'MASCULINE_ORDINAL_INDICATOR' => '00BA',
+			'RIGHT_DOUBLE_ANGLE' => '00BB',
+			'ONE_QUARTER' => '00BC',
+			'ONE_HALF' => '00BD',
+			'THREE_QUARTERS' => '00BE',
+			'INVERT_QUESTION' => '00BF',
+			'MULTIPLICATION_SIGN' => '00D7',
+			'DIVISION_SIGN' => '00F7'
 	);
 	
 	/**
